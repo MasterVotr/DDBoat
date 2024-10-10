be6c5d0b6edd9a0e4aa476c02d7145372768c599
Johannes von Grundherr <74217475+Johannes-ece@users.noreply.github.com>
2024-10-10 20:41:54 +0200

    big change

--- ddboat.py
+++ ddboat.py
@@ -33,10 +33,6 @@ import gpxpy.gpx
 import simplekml
 from datetime import datetime
 
-
-b = np.array([[6.],[1468.],[-4455.]])
-A = np.array([[[-73.05261869,-5.55853209,-7.24944167],[-1.10974578,61.19152157,4.77293562],[8.48208413,1.2818979,-66.35225166]]])
-
 # Access to the drivers (adjust the path as needed)
 sys.path.append(os.path.join(os.path.dirname(__file__), 'drivers-ddboat-v2'))
 
@@ -60,6 +56,55 @@ projDegree2Meter = Proj(
 # Magnetic correction
 b = np.array([[6.],[1468.],[-4455.]])
 A = np.array([[[-73.05261869,-5.55853209,-7.24944167],[-1.10974578,61.19152157,4.77293562],[8.48208413,1.2818979,-66.35225166]]])
+
+class Coordinate:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def distance_to(self, other: 'Coordinate') -> float:
+        """
+        Calculate the Euclidean distance to another coordinate.
+        
+        Parameters:
+        - other (Coordinate): The other coordinate.
+
+        Returns:
+        - float: Distance to the other coordinate.
+        """
+        return np.hypot(self.x - other.x, self.y - other.y)
+
+    def angle_to(self, other: 'Coordinate', current_heading: float) -> float:
+        """
+        Calculate the angle from the current heading to another coordinate.
+        
+        Parameters:
+        - other (Coordinate): The other coordinate.
+        - current_heading (float): Current heading of the boat in degrees.
+
+        Returns:
+        - float: Angle from current heading to the other coordinate in degrees (-180 to 180).
+        """
+        dx = other.x - self.x
+        dy = other.y - self.y
+        target_heading = np.degrees(np.arctan2(dy, dx))
+        target_heading = 90 - target_heading  # Convert to geographic angle
+        if target_heading < 0:
+            target_heading += 360
+        
+        angle_diff = target_heading - current_heading
+        if angle_diff > 180:
+            angle_diff -= 360
+        elif angle_diff < -180:
+            angle_diff += 360
+        
+        return angle_diff
+
+    def __str__(self) -> str:
+        return f"Coordinate(x={self.x}, y={self.y})"
+
+    def __repr__(self) -> str:
+        return self.__str__()
 
 def cvt_gll_ddmm_2_dd(gll_data):
     """
@@ -122,10 +167,10 @@ def convert_to_utm(lat, lon):
     - lon (float): Longitude in decimal degrees.
 
     Returns:
-    - tuple: (x, y) coordinates in meters.
+    - Coordinate: UTM coordinates as a Coordinate instance.
     """
     x, y = projDegree2Meter(lon, lat)
-    return x, y
+    return Coordinate(x, y)
 
 def initialize_imu():
     """
@@ -185,29 +230,6 @@ def compute_compass_heading(mag_calib):
     if heading_deg < 0:
         heading_deg += 360
     return heading_deg
-
-
-def calculate_distance_and_heading(x, y, ref_x, ref_y):
-    """
-    Calculate the distance and heading from the current position to a reference point.
-
-    Parameters:
-    - x (float): Current x coordinate in meters.
-    - y (float): Current y coordinate in meters.
-    - ref_x (float): Reference x coordinate in meters.
-    - ref_y (float): Reference y coordinate in meters.
-
-    Returns:
-    - tuple: (distance in meters, heading in degrees)
-    """
-    dx = x - ref_x
-    dy = y - ref_y
-    distance = np.hypot(dx, dy)
-    heading_trigo = np.degrees(np.arctan2(dy, dx))
-    heading_geo = 90.0 - heading_trigo  # Convert to geographic angle
-    if heading_geo < 0:
-        heading_geo += 360
-    return distance, heading_geo
 
 def initialize_motors():
     """
@@ -270,7 +292,7 @@ def log_to_kml(lat_lon_list, filename='gps_data.kml'):
         kml.newpoint(name="GPS_{}".format(i), coords=[(lon, lat)])
     kml.save(filename)
 
-def navigation(gps, imu, ref_x, ref_y, lat_lon_list):
+def navigation(gps, imu, ref_coord: Coordinate, lat_lon_list):
     # Read GPS and compass data
     # Read GPS data
     gll_ok, gll_data = gps.read_gll_non_blocking()
@@ -280,13 +302,9 @@ def navigation(gps, imu, ref_x, ref_y, lat_lon_list):
         lon = float(int(ilon/100)) + (ilon%100)/60
         if gll_data[3] == "W":
             lon = -lon
-        x, y = projDegree2Meter(lon, lat)
-        dx = x - ref_x
-        dy = y - ref_y
-        distance = np.sqrt(dx*dx + dy*dy)
-        gps_heading = 90.0 - np.degrees(np.arctan2(dy, dx))
-        while gps_heading < 0:
-            gps_heading += 360
+        current_coord = convert_to_utm(lat, lon)
+
+        distance = current_coord.distance_to(ref_coord)
 
         # Read IMU data
         imu_data = read_imu_data(imu)
@@ -296,53 +314,56 @@ def navigation(gps, imu, ref_x, ref_y, lat_lon_list):
         mag_calib = apply_compass_calibration(mag_raw)
         compass_heading = compute_compass_heading(mag_calib)
 
+        # Calculate angle to reference point
+        angle_to_ref = current_coord.angle_to(ref_coord, compass_heading)
+
         # Print outputs
         print("Timestamp: {}".format(datetime.now()))
         print("Position: ({:.6f}, {:.6f})".format(lat, lon))
         print("Distance to Reference Point: {:.2f} meters".format(distance))
-        print("GPS Heading to Reference Point: {:.2f} degrees".format(gps_heading))
+        print("Angle to Reference Point: {:.2f} degrees".format(angle_to_ref))
         print("Compass Heading: {:.2f} degrees".format(compass_heading))
         print(80 * "-")
 
         # Append GPS data to list
         lat_lon_list.append((lat, lon))
 
-    return x, y, compass_heading
+    return current_coord, compass_heading
 
 def main_example():
     """
     Main function demonstrating the usage of the utility functions.
     """
     # Initialize modules
-    gps = gpddrv.GpsIO()
-    gps.set_filter_speed("0")  # Allow GPS measures when stationary
+    gps = initialize_gps()
     imu = initialize_imu()
-    ard = arddrv.ArduinoIO()
+    ard = initialize_motors()
 
     # Reference point (example coordinates)
     ref_lat = 48.199111
     ref_lon = -3.014930
-    ref_x, ref_y = projDegree2Meter(ref_lon, ref_lat)
+    ref_coord = convert_to_utm(ref_lat, ref_lon)
 
     # Goal point (example coordinates)
     goal_lat = ref_lat
     goal_lon = ref_lon
-    goal_x, goal_y = projDegree2Meter(goal_lon, goal_lat)
+    goal_coord = convert_to_utm(goal_lat, goal_lon)
 
     # Lists to store GPS data
     lat_lon_list = []
 
     while True:
         # Navigation
-        x, y, cur_heading = navigation(gps, imu, ref_x, ref_y, lat_lon_list)
+        current_coord, cur_heading = navigation(gps, imu, ref_coord, lat_lon_list)
 
         # Guidance
-        d, new_heading = calculate_distance_and_heading(x, y, goal_x, goal_y)
-        heading_err = new_heading - cur_heading
+        distance_to_goal = current_coord.distance_to(goal_coord)
+        angle_to_goal = current_coord.angle_to(goal_coord, cur_heading)
 
         # Control
-
-
+        # Add your control logic here
+        print(f"Distance to goal: {distance_to_goal:.2f} meters")
+        print(f"Angle to goal: {angle_to_goal:.2f} degrees")
 
         time.sleep(1)
 
@@ -354,9 +375,6 @@ def main_example():
     # set_motor_speeds(ard, 100, -100)  # Rotate in place
     # time.sleep(2)
     # stop_motors(ard)
-
-
-
 
 if __name__ == '__main__':
     main_example()
